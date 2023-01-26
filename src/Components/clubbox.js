abfd6d4d639b3c1ef5cc753d93b36d45e03bfd0d
Serhan Desteli <iserhan2001@gmail.com>
2023-01-26 08:34:51 +0300

    Fix

--- src/Components/clubbox.js
+++ src/Components/clubbox.js
@@ -4,7 +4,7 @@ function ClubBox(props) {
 	return (
 		<div>
 			<h3>{props.text}</h3>
-			<img src='https://placehold.jp/100x100.png' alt='img'></img>
+			<img src='https://placehold.jp/100x100.png' alt='imga'></img>
 		</div>
 	);
 }
