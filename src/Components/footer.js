c48660a857bdc2815d8604e21fdd63b689de67a8
Serhan Desteli <iserhan2001@gmail.com>
2023-03-13 22:07:58 +0300

    Revize footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,55 +1,60 @@
-import React from 'react';
+import './styles.css';
 
 function Footer() {
 	return (
-		<span className='footerContainer'>
-			© Bu websitesi&nbsp;
-			<a
-				className='nameContainer'
-				target='_blank'
-				rel='noreferrer'
-				href='https://www.linkedin.com/in/iserhand/'
-			>
-				İbrahim Serhan Desteli
-			</a>
-			,&nbsp;
-			<a
-				className='nameContainer'
-				target='_blank'
-				rel='noreferrer'
-				href='https://www.linkedin.com/in/bilgehanerten/'
-			>
-				Bilgehan Erten
-			</a>
-			,&nbsp;
-			<a
-				className='nameContainer'
-				target='_blank'
-				rel='noreferrer'
-				href='https://www.linkedin.com/in/elçin-kılıç-2345351b5/'
-			>
-				Elçin Kılıç
-			</a>
-			&nbsp;ve&nbsp;
-			<a
-				className='nameContainer'
-				target='_blank'
-				rel='noreferrer'
-				href='https://www.linkedin.com/in/burak-onuralp-bozkurt-a0854219a/'
-			>
-				Burak Bozkurt
-			</a>
-			&nbsp;tarafından&nbsp;
-			<a
-				className='nameContainer'
-				target='_blank'
-				rel='noreferrer'
-				href='https://www.instagram.com/jamination.official/'
-			>
-				Jamination
-			</a>
-			&nbsp;adına geliştirilmiştir.
-		</span>
+		<div className='footerContainer'>
+			<p className='footerText'>
+				© Bu websitesi{' '}
+				<a
+					className='nameContainer'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.linkedin.com/in/iserhand/'
+				>
+					İbrahim Serhan Desteli
+				</a>
+				,{' '}
+				<a
+					className='nameContainer'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.linkedin.com/in/bilgehanerten/'
+				>
+					Bilgehan Erten
+				</a>
+				,{' '}
+				<a
+					className='nameContainer'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.linkedin.com/in/elçin-kılıç-2345351b5/'
+				>
+					Elçin Kılıç
+				</a>{' '}
+				ve{' '}
+				<a
+					className='nameContainer'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.linkedin.com/in/burak-onuralp-bozkurt-a0854219a/'
+				>
+					Burak Bozkurt
+				</a>{' '}
+				tarafından{'  '}
+				<a
+					className='nameContainer'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.instagram.com/jamination.official/'
+				>
+					Jamination
+				</a>{' '}
+				adına geliştirilmiştir.
+			</p>
+			<div className='linkContainer'></div>
+			<div className='linkContainer'></div>
+		</div>
 	);
 }
+
 export default Footer;
